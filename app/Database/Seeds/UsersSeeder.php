b2a9dd1085c4e1c81416dc4376c7bacc2d27c81d
Erza Putra Albasori <erzaputra9110@gmail.com>
2020-06-03 19:18:07 +0700

    fix: encrypt user seeder

--- app/Database/Seeds/UsersSeeder.php
+++ app/Database/Seeds/UsersSeeder.php
@@ -7,7 +7,7 @@ class UsersSeeder extends \CodeIgniter\Database\Seeder
                 $this->db->table('users')->insert([
                   'email' => 'admin@si-japra.com',
                   'nim' => 'admin@si-japra',
-                  'password' => 'erzagantengidaman',
+                  'password' => md5('erzagantengidaman'),
                   'name' => 'Admin SI - JAPRA',
                   'major' => 'S2 Teknik Nuklir',
                   'role' => 'admin'
